3b7faa023655797d7cf96981b3123315c26d29ce
Dirk Chang <dirk@kooiot.com>
2020-10-14 18:30:07 +0800

    fixed the mkdir asserts

--- docker_debian_builder/download_toolchains.sh
+++ docker_debian_builder/download_toolchains.sh
@@ -8,7 +8,7 @@ DL_VER="toolchain_v1"
 DL_URL="https://github.com/freeioe/freeioe_dockers/releases/download/"${DL_VER}
 DL_FILE="toolchains.tar"
 
-mkdir ${TL_DIR}
+mkdir -p ${TL_DIR}
 cd ${TL_DIR}
 
 wget ${DL_URL}/${DL_FILE} -O ${DL_FILE}
