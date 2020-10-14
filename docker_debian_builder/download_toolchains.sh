abe4eb3a0e8e5a83cbd1bfd4eba9078a924d7442
Dirk Chang <dirk@kooiot.com>
2020-10-14 18:21:21 +0800

    fixed scripts

--- docker_debian_builder/download_toolchains.sh
+++ docker_debian_builder/download_toolchains.sh
@@ -1,1 +1,17 @@
-../docker_toolchains/download_toolchains.sh
+#!/bin/sh
+
+set -e
+
+TL_DIR="/"
+
+DL_VER="toolchain_v1"
+DL_URL="https://github.com/freeioe/freeioe_dockers/releases/download/"${DL_VER}
+DL_FILE="toolchains.tar"
+
+mkdir ${TL_DIR}
+cd ${TL_DIR}
+
+wget ${DL_URL}/${DL_FILE} -O ${DL_FILE}
+
+tar xvf ${DL_FILE}
+rm ${DL_FILE}
