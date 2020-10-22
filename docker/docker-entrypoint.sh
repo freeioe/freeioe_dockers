b4f5bc2759a738f018b6eadf18f6c4e8819eadde
Dirk Chang <dirk@kooiot.com>
2020-10-22 18:30:56 +0800

    compare the version

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,32 +4,62 @@ set -e
 
 mkdir -p /usr/ioe/ipt
 
-if [ -f /usr/ioe_ipt/strip_done ]; then
-	echo "File released!!"
-else
-	if [ -f /usr/ioe/skynet/cfg.json ]; then
-		cp /usr/ioe/skynet/cfg.json /usr/ioe/ipt/
-		cp /usr/ioe/skynet/cfg.json.md5 /usr/ioe/ipt/
-	fi
-
+uncompress_files() {
+	# UnCompress FreeIOE
 	if [ -f /usr/ioe_ipt/freeioe.tar.gz ]; then
-		mv -f /usr/ioe_ipt/freeioe.tar.gz* /usr/ioe/ipt/
-		rm -rf /usr/ioe/freeioe
-		mkdir -p /usr/ioe/freeioe
-		tar -C /usr/ioe/freeioe -xzf /usr/ioe/ipt/freeioe.tar.gz
-	fi
-	if [ -f /usr/ioe_ipt/skynet.tar.gz ]; then
-		mv -f /usr/ioe_ipt/skynet.tar.gz* /usr/ioe/ipt/
-		rm -rf /usr/ioe/skynet
-		mkdir -p /usr/ioe/skynet
-		tar -C /usr/ioe/skynet -xzf /usr/ioe/ipt/skynet.tar.gz
-		if [ -f /usr/ioe/ipt/cfg.json ]; then
-			cp /usr/ioe/ipt/cfg.json* /usr/ioe/skynet/
+		mkdir -p /usr/ioe/freeioe_new
+		tar -C /usr/ioe/freeioe_new -xzf /usr/ioe_ipt/freeioe.tar.gz
+
+		if [ -f /usr/ioe/freeioe/version ]; then
+			IPT_VERSION=`head -n1 /usr/ioe/freeioe_new/version`
+			INS_VERSION=`head -n1 /usr/ioe/freeioe/version`
+
+			if [ ${IPT_VERSION} -gt ${INS_VERSION} ]; then
+				rm -rf /usr/ioe/freeioe
+				mv /usr/ioe/freeioe_new /usr/ioe/freeioe
+				rm -f /usr/ioe/ipt/freeioe.tar.gz*
+				cp /usr/ioe_ipt/freeioe.tar.gz* /usr/ioe/ipt/
+			else
+				rm -rf /usr/ioe/freeioe_new
+			fi
+		else
+			mv /usr/ioe/freeioe_new /usr/ioe/freeioe
+			rm -f /usr/ioe/ipt/freeioe.tar.gz*
+			cp /usr/ioe_ipt/freeioe.tar.gz* /usr/ioe/ipt/
 		fi
 	fi
 
-	date > /usr/ioe_ipt/strip_done
-fi
+	# UnCompress Skynet
+	if [ -f /usr/ioe_ipt/skynet.tar.gz ]; then
+		mkdir -p /usr/ioe/skynet_new
+		tar -C /usr/ioe/skynet -xzf /usr/ioe_ipt/skynet.tar.gz
+
+		if [ -f /usr/ioe/skynet/cfg.json ]; then
+			cp /usr/ioe/skynet/cfg.json /usr/ioe/skynet_new/
+			cp /usr/ioe/skynet/cfg.json.md5 /usr/ioe/skynet_new/
+		fi
+
+		if [ -f /usr/ioe/skynet/version ]; then
+			IPT_VERSION=`head -n1 /usr/ioe/skynet_new/version`
+			INS_VERSION=`head -n1 /usr/ioe/skynet_new/version`
+
+			if [ ${IPT_VERSION} -gt ${INS_VERSION} ]; then
+				rm -rf /usr/ioe/skynet
+				mv /usr/ioe/skynet_new /usr/ioe/skynet
+				rm -f /usr/ioe/ipt/skynet.tar.gz*
+				cp /usr/ioe_ipt/skynet.tar.gz* /usr/ioe/ipt/
+			else
+				rm -rf /usr/ioe/skynet_new
+			fi
+		else
+			mv /usr/ioe/skynet_new /usr/ioe/skynet
+			rm -f /usr/ioe/ipt/skynet.tar.gz*
+			cp /usr/ioe_ipt/skynet.tar.gz* /usr/ioe/ipt/
+		fi
+	fi
+}
+
+uncompress_files
 
 cd /usr/ioe/skynet
 
